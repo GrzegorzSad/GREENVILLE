13cf56c6c485c4839a18c50c07ebd2d5cc1ef804
Lew ZeroJeden <N00223252@iadt.ie>
2023-12-06 01:00:58 +0000

    in the process of a rework

---
+++ js.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('input[type="radio"]').click(function(){
+        if($(this).attr("id") == "flexRadioDefault1"){
+            $("#clickAndCollectContent").removeClass("d-none");
+            $("#officeCateringContent").addClass("d-none");
+        }
+        if($(this).attr("id") == "flexRadioDefault2"){
+            $("#officeCateringContent").removeClass("d-none");
+            $("#clickAndCollectContent").addClass("d-none");
+        }
+    });
+});
