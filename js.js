3a50e0bad7e67d6cbeb740a9f152f0447260e056
Lew ZeroJeden <N00223252@iadt.ie>
2023-12-10 17:31:54 +0000

    menu work is being worked

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
-$(document).ready(function(){
-    $('input[type="radio"]').click(function(){
-        if($(this).attr("id") == "flexRadioDefault1"){
+$(document).ready(function () {
+    $('input[type="radio"]').click(function () {
+        if ($(this).attr("id") == "flexRadioDefault1") {
             $("#clickAndCollectContent").removeClass("d-none");
             $("#officeCateringContent").addClass("d-none");
         }
-        if($(this).attr("id") == "flexRadioDefault2"){
+        if ($(this).attr("id") == "flexRadioDefault2") {
             $("#officeCateringContent").removeClass("d-none");
             $("#clickAndCollectContent").addClass("d-none");
         }
